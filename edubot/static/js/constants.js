1e11e171654904141f636b1bfe3a87129f6fd954
sathishlakshmanan <60668130+sathishlakshmanan@users.noreply.github.com>
2022-04-19 00:57:34 +0200

    Update constants.js

--- edubot/static/js/constants.js
+++ edubot/static/js/constants.js
@@ -1,3 +1,3 @@
 const action_name = "action_give_show_video";
-const rasa_server_url = "https://d444-37-24-50-9.ngrok.io/webhooks/rest/webhook";
+const rasa_server_url = "https://5428-37-24-50-9.ngrok.io/webhooks/rest/webhook";
 const sender_id = uuidv4();
